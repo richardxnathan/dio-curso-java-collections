3023d5501d4300983a2f60cfb06c7a7bd4d57031
richardxnathan <richardnathand@gmail.com>
2024-06-25 18:33:56 -0300

    List: NumbersSum

---
+++ src/list/research/numberssum/NumbersSum.java
@@ -0,0 +1,52 @@
+package src.list.research.numberssum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumbersSum {
+
+    private List<Integer> inteiros = new ArrayList<>();
+
+    public void addNumber(int number){
+        inteiros.add(number);
+    }
+
+    public int sum(){
+        int total = 0;
+
+        for (int i : inteiros) {
+            total += i;
+        }
+
+        return total;
+    }
+
+    public int findGreatestNumber(){
+        int number = inteiros.get(0);
+
+        for (int i : inteiros) {
+            if (i > number){
+                number = i;
+            }
+        }
+
+        return number;
+    }
+
+    public int findSmallNumber(){
+        int number = inteiros.get(0);
+
+        for (int i : inteiros) {
+            if (i < number){
+                number = i;
+            }
+        }
+
+        return number;
+    }
+
+    public void showNumbers(){
+        System.out.println(inteiros);
+    }
+
+}
